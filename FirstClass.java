0eb84a045dcbfcf34cb468619c2ad24b4dc7d8a9
Priyanshu6926 <priyanshushingole@gmail.com>
2026-06-08 14:29:00 +0530

    first commit

---
+++ FirstClass.java
@@ -0,0 +1,125 @@
+import java.util.Scanner;
+
+/** 
+class FirstClass {
+    public static void main(String args[]) {
+        System.out.println("*\n** ");
+    }
+}
+
+
+class FirstClass {
+    public static void main(String args[]) {
+        //VARIABLE
+        
+        int a = 20;
+        int b = 10;
+        int sum = a+b;
+        // System.out.println(sum);
+        int difference = a-b;
+        // System.out.print(difference);
+        int product = a*b;
+        // System.out.println(product);
+
+        int divide = a/b;
+        // System.out.println(divide);
+        int ans = a*b / a-b;
+        int ans1 = (a*b) / (a-b);
+        System.out.println(ans);
+        System.out.println(ans1);
+
+
+
+    }
+}
+
+
+
+import java.util.*;
+
+public class FirstClass {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        //nextLine
+        //nextFloat()
+        //nextInt
+
+        System.out.println(name);
+
+    }
+}
+    
+
+
+import java.util.*;
+
+public class FirstClass {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
+
+
+import java.util.*;
+public class FirstClass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if (age > 18) {
+            System.out.println("Adult");
+        }
+        else {
+            System.out.println("Not Adult");
+        }
+        
+
+    }
+
+}
+
+
+
+import java.util.*;
+public class FirstClass {
+        public static void main(String agrs[]) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if (x % 2 == 0) {
+            System.out.println("Even");
+        }
+        else {
+            System.out.println("Odd");
+        }
+    }
+
+}
+*/
+
+import java.util.*;
+public class FirstClass{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int button = sc.nextInt();
+
+        if (button == 1){
+            System.out.println("Hello");
+        } else if (button == 2) {
+            System.out.println("Namaste");
+        } else if (button == 3) {
+            System.out.println("Bonjour");
+        } else {
+            System.out.println("Invalid");
+        }
+    }
+}
+*/
+
+
+
+
+
