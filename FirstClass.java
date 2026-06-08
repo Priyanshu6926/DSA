bd2fd7b0e7d3f3e1004db1178b05deced1783b0d
Priyanshu6926 <priyanshushingole@gmail.com>
2026-06-08 15:19:34 +0530

    calculator

--- FirstClass.java
+++ FirstClass.java
@@ -140,7 +140,7 @@ public class FirstClass{
     }
 }  
 
-*/
+
 
 import java.util.*;
 public class FirstClass {
@@ -195,3 +195,35 @@ public class FirstClass {
 
 
 
+import java.util.*;
+public class FirstClass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int operator = sc.nextInt();
+        switch (operator) {
+            case 1:
+                System.out.println(a+b);
+                break;
+            case 2:
+                System.out.println(a-b);
+                break;
+            case 3:
+                System.out.println(a*b);
+                break;
+            case 4:
+                System.out.println(a/b);
+                break;
+            case 5:
+            System.out.println(a%b);
+            break;
+            default:
+            System.out.println("Invalid");
+        
+        }
+    }
+
+}
+
+*/
