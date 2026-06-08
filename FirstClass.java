70b88a6dc721dac53cd1bf2e76315979a1775d5d
Priyanshu6926 <priyanshushingole@gmail.com>
2026-06-08 14:41:16 +0530

    calender

--- FirstClass.java
+++ FirstClass.java
@@ -117,7 +117,6 @@ public class FirstClass{
         }
     }
 }
-*/
 
 import java.util.*;
 public class FirstClass{
@@ -141,6 +140,58 @@ public class FirstClass{
     }
 }  
 
+*/
+
+import java.util.*;
+public class FirstClass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner (System.in);
+        int button = sc.nextInt();
+        switch(button) {
+            case 1:
+                System.out.println("Jan");
+                break;
+            case 2:
+                System.out.println("Feb");
+                break;
+            case 3:
+                System.out.println("Mar");
+                break;
+            case 4:
+                System.out.println("Apr");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("Jun");
+                break;
+            case 7:
+                System.out.println("Jul");
+                break;
+            case 8:
+                System.out.println("Aug");
+                break;
+            case 9:
+                System.out.println("Sep");
+                break;
+            case 10:
+                System.out.println("Oct");
+                break;
+            case 11:
+                System.out.println("Nov");
+                break;
+            case 12:
+                System.out.println("Dec");
+                break;
+            default:
+                System.out.println("Invalid");
+                break;
+            
+        }
+
+    }
+}
 
 
 
