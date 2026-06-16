4d95605ef000acd67eaa17d170fec2583680cd99
Priyanshu6926 <priyanshushingole@gmail.com>
2026-06-16 16:31:09 +0530

    question

--- functions.java
+++ functions.java
@@ -53,4 +53,62 @@ public class functions{
     } 
 }
 
+
+// factorial
+
+import java.util.*;
+public class functions {
+    public static void printFactorial(int n){
+        int factorial = 1;
+        for (int i = n; i >=1; i--){
+            factorial = factorial * i ;
+        }
+        System.out.println(factorial);
+        return;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        printFactorial(n);
+    }
+    
+}
+ 
+import java.util.*;
+public class functions {
+    public static int printAvg(int a,int b, int c){
+        int avg = (a+b+c)/3;
+        return avg;
+    }
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(printAvg(a,b,c));
+        sc.close();
+        
+    }
+}
 */
+//Write a function to print the sum of all odd numbers from 1 to n.
+
+import java.util.*;
+public static int printSumofOdd(int n){
+    int sum = 0;
+    for( int i=1;i<=n;i++){
+        if (i%2 != 0){
+            sum = sum + i;
+        }}
+            return sum;
+        }
+    
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(printSumofOdd(n));
+        sc.close();
+    }    
+
+
