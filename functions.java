95c1499231776aba8396d30b50718ec0e3be1e33
Priyanshu6926 <priyanshushingole@gmail.com>
2026-06-16 13:12:15 +0530

    print name

--- functions.java
+++ functions.java
@@ -1,3 +1,17 @@
+// returnType functionName(type arg1, type arg2, ...)
+// operations
+// return 
+
+import java.util.*;
 public class functions {
-    
+    public static void printMyName(String name) {
+        System.out.println(name);
+        return;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        printMyName(name);
+    }
 }
