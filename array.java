9bd2abbf89a50d0cee325b166e4342c12ea85b4b
Priyanshu6926 <priyanshushingole@gmail.com>
2026-06-22 15:53:37 +0530

    2DArray

--- array.java
+++ array.java
@@ -40,7 +40,7 @@ public class arrays {
         }
     }
 }
-*/ 
+
 
 import java.util.*;
 public class arrays {
@@ -64,3 +64,22 @@ public class arrays {
     }
 }
 }
+*/ 
+//Take an array of names as input from the user and print them on the screen.
+import java.util.*;
+public class array {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        String names[] = new String[size];
+
+        for (int i=0;i<size;i++){
+            names [i] = sc.next();
+            
+        }
+        for (int i=0;i<names.length;i++){
+            System.out.println(names[i]);
+        }
+    }
+}
+
