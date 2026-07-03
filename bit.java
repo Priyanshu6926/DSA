7113be0baeac22c66a9ccf77ff1201996389e404
Priyanshu6926 <priyanshushingole@gmail.com>
2026-07-03 17:36:55 +0530

    power of 2

--- bit.java
+++ bit.java
@@ -43,7 +43,7 @@ public class bit {
     }
 }
 
-*/
+
 import java.util.*;
 public class bit {
     public static void main(String args[]){
@@ -65,4 +65,21 @@ public class bit {
             System.out.println(newNumber);
         }
     }
+}
+*/
+
+//Write a program to find if a number is a power of 2 or not.
+import java.util.*;
+public class bit{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n > 0 && (n&(n-1)) == 0 )
+            {
+                System.out.println("Yes it is a power of 2");
+            }
+        else{
+            System.out.println("No it is not a power of 2");
+        }
+    }ß
 }
