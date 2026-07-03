54c0ead8ee48f4d731968ad1901dd4a7688d9185
Priyanshu6926 <priyanshushingole@gmail.com>
2026-07-03 16:17:03 +0530

    update bit

--- bit.java
+++ bit.java
@@ -16,7 +16,7 @@ public class bit {
     }
 }
 
-*/
+
 import java.util.*;
 public class bit {
     public static void main(String args[]){
@@ -26,5 +26,43 @@ public class bit {
         
         int newNumber = bitMask | n;
         System.out.println(newNumber);
+    }
+}
+
+
+import java.util.*;
+public class bit {
+    public static void main(String args[]){
+        int n = 5;
+        int pos = 2  ;
+        int bitMask = 1 << pos;
+        int notBitMask = ~(bitMask);
+        
+        int newNumber = notBitMask & n;
+        System.out.println(newNumber);
+    }
+}
+
+*/
+import java.util.*;
+public class bit {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int oper = sc.nextInt(); 
+
+        //oper = 1 set bit , oper = 0 clear bit
+        int n = 5;
+        int pos = 1;
+
+        int bitMask = 1<<pos;
+        if(oper == 1){
+            int newNumber = bitMask | n;
+            System.out.println(newNumber);
+        }
+        else{
+            int newBitMask = ~(bitMask);
+            int newNumber =newBitMask & n;
+            System.out.println(newNumber);
+        }
     }
 }
