870becd122995f40c8c77d49be075a6e7f53dece
Priyanshu6926 <priyanshushingole@gmail.com>
2026-07-04 13:42:44 +0530

    selection sort

--- sorting.java
+++ sorting.java
@@ -1,3 +1,5 @@
+/*** 
+
 import java.util.*;
 public class sorting {
     public static void printArray(int arr[]){
@@ -23,3 +25,35 @@ public class sorting {
         printArray(arr);
     }
 }
+
+*/
+// selection sort.  (On^2)
+
+import java.util.*;
+public class sorting {
+    public static void printArray(int arr[]){
+        for (int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {7,8,3,1,2};
+
+        //selection sort.  (On^2)
+        for(int i = 0;i<arr.length-1;i++){
+        int smallest = i;
+            for(int j = i;j<arr.length;j++){
+                if(arr[smallest] > arr[j])   {
+                    smallest = j;
+                    }
+                }
+            //swap
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+            }
+        printArray(arr);
+    }
+}
+
