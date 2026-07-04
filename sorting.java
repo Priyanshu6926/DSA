70d1161235f33e2551bdc67d43664a481b276b7a
Priyanshu6926 <priyanshushingole@gmail.com>
2026-07-04 13:51:50 +0530

    insertion sort

--- sorting.java
+++ sorting.java
@@ -26,7 +26,7 @@ public class sorting {
     }
 }
 
-*/
+
 // selection sort.  (On^2)
 
 import java.util.*;
@@ -57,3 +57,29 @@ public class sorting {
     }
 }
 
+*/ 
+
+
+import java.util.*;
+public class sorting {
+    public static void printArray(int arr[]){
+        for (int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {7,8,3,1,2};
+// insertion sort
+for(int i = 1;i<arr.length;i++){
+    int current = arr[i];
+    int j = i-1;
+    while(j>=0 && arr[j] > current){
+        arr[j+1] = arr[j];
+        j--;
+    }
+    arr[j+1] = current;
+}
+        printArray(arr);
+    }
+}   
