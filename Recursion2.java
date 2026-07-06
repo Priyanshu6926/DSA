0e70e8827ad3132d169cef0b530d4ff3485e4cd3
Priyanshu6926 <priyanshushingole@gmail.com>
2026-07-06 16:58:22 +0530

    reverse

--- Recursion2.java
+++ Recursion2.java
@@ -1,11 +1,36 @@
+//tower of hanoi.  O(2^n)
+/***
 public class Recursion2 {
-    public static void towerofhanoi(int n, char source, char destination, char auxiliary) {
-        if (n == 1) {
-            System.out.println("Move disk 1 from " + source + " to " + destination);
+    public static void towerofhanoi(int n, String src, String helper,String dest){
+        if (n==1){
+            System.out.println("transfer disk" +n+ "from"+src+"to"+dest);
+        return;
+        }
+        towerofhanoi(n-1,src,dest,helper);
+        System.out.println("transfer disk" + n + "from" + src + "to" + dest);
+        towerofhanoi(n-1,helper,src,dest);
+    }
+    public static void main(String[] args) {
+        int n=4;
+        towerofhanoi(n,"S","H","D");
+        
+    }
+}
+
+*/
+
+ // reverse of a string using recursion. O(n)
+public class Recursion2{
+    public static void printRev(String str,int index){
+        if(index == 0){
+            System.out.println(str.charAt(index));
             return;
         }
-        towerofhanoi(n - 1, source, auxiliary, destination);
-        System.out.println("Move disk " + n + " from " + source + " to " + destination);
-        towerofhanoi(n - 1, auxiliary, destination, source);
+        System.out.println(str.charAt(index));
+        printRev(str,index-1);
+    }
+    public static void main(String args[]){
+        String str = "qwerty";
+        printRev(str,str.length()-1);
     }
 }
