4fcdb9510fabd962a8aad6bc1f45933fd0cf0114
Priyanshu6926 <priyanshushingole@gmail.com>
2026-07-08 23:49:39 +0530

    reursion

--- Recursion2.java
+++ Recursion2.java
@@ -73,7 +73,7 @@ public class Recursion2{
         if(idx == arr.length-1){
             return true;
         }
-        if (arr[idx]<arr[idx+1]){
+        if (arr[idx] < arr[idx+1]){
             return isSorted(arr,idx+1);
         }else{
             return false;
