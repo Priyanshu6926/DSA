2ff15f474a2c882c802828fa070adea4a9ca462f
Priyanshu6926 <priyanshushingole@gmail.com>
2026-07-09 00:42:19 +0530

    n queens

--- recursion.java
+++ recursion.java
@@ -1,3 +1,6 @@
+//premutation of string 
+/*** 
+
 public class recursion {
     public static void printPermutation(String str,int inx,String prem){
         if(str.length() == 0){
@@ -11,7 +14,101 @@ public class recursion {
         }
     }
     public static void main(String args[]){
-        String str = "abcde";
+        String str = "abcd";
         printPermutation(str,0,"");
     }
+}
+*/
+
+// N queens problem using recursion.Time complexity - O(n^n)
+class Recursion {
+   public boolean isSafe(int row, int col, char[][] board) {
+       //horizontal
+       for(int j=0; j<board.length; j++) {
+           if(board[row][j] == 'Q') {
+               return false;
+           }
+       }
+      
+       //vertical
+       for(int i=0; i<board.length; i++) {
+           if(board[i][col] == 'Q') {
+               return false;
+           }
+       }
+      
+       //upper left
+       int r = row;
+       for(int c=col; c>=0 && r>=0; c--, r--) {
+           if(board[r][c] == 'Q') {
+               return false;
+           }
+       }
+      
+       //upper right
+       r = row;
+       for(int c=col; c<board.length && r>=0; r--, c++) {
+           if(board[r][c] == 'Q') {
+               return false;
+           }
+       }
+      
+       //lower left
+       r = row;
+       for(int c=col; c>=0 && r<board.length; r++, c--) {
+           if(board[r][c] == 'Q') {
+               return false;
+           }
+       }
+      
+       //lower right
+       for(int c=col; c<board.length && r<board.length; c++, r++) {
+           if(board[r][c] == 'Q') {
+               return false;
+           }
+       }
+      
+       return true;
+   }
+  
+   public void saveBoard(char[][] board, list<list<String>> allBoards) {
+       String row = "";
+       list<String> newBoard = new Arraylist<>();
+      
+       for(int i=0; i<board.length; i++) {
+           row = "";
+           for(int j=0; j<board[0].length; j++) {
+               if(board[i][j] == 'Q')
+                   row += 'Q';
+               else
+                   row += '.';
+           }
+           newBoard.add(row);
+       }
+      
+       allBoards.add(newBoard);
+   }
+  
+   public void helper(char[][] board, List<List<String>> allBoards, int col) {
+       if(col == board.length) {
+           saveBoard(board, allBoards);
+           return;
+       }
+      
+       for(int row=0; row<board.length; row++) {
+           if(isSafe(row, col, board)) {
+               board[row][col] = 'Q';
+               helper(board, allBoards, col+1);
+               board[row][col] = '.';
+           }
+       }
+   }
+  
+   public List<List<String>> solveNQueens(int n) {
+       List<List<String>> allBoards = new ArrayList<>();
+       char[][] board = new char[n][n];
+      
+       helper(board, allBoards, 0);
+       return allBoards;
+   }
 }
