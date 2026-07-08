fbac1a347a5fbd884f7e01ad27209e95cfb5c570
Priyanshu6926 <priyanshushingole@gmail.com>
2026-07-08 23:54:44 +0530

    premutation

--- recursion.java
+++ recursion.java
@@ -1,3 +1,17 @@
 public class recursion {
-    public
+    public static void printPermutation(String str,int inx,String prem){
+        if(str.length() == 0){
+            System.out.println(prem);
+            return;
+        }
+        for(int i=0; i<str.length(); i++){
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0 ,i) + str.substring(i+1);
+            printPermutation(newStr,inx+1,prem+currChar);
+        }
+    }
+    public static void main(String args[]){
+        String str = "abcde";
+        printPermutation(str,0,"");
+    }
 }
