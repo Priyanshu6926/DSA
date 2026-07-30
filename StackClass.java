bd9f33099f35a2ac1c982b7afed8e0e565a40fd8
Priyanshu6926 <priyanshushingole@gmail.com>
2026-07-31 01:00:44 +0530

    push at bottom

--- StackClass.java
+++ StackClass.java
@@ -111,16 +111,46 @@
 
 
 // JAVA COLLECETION Framnwork
+// import java.util.*;
+// public class StackClass{
+//     public static void main(String[] args) {
+//         Stack<Integer> s = new Stack<>();
+//         s.push(1);
+//         s.push(2);
+//         s.push(3);
+//         while (!s.isEmpty()) {
+//             System.out.println(s.peek());
+//             s.pop();
+//         }
+//     }
+// }
+
+// push at bottom using recursion
+
 import java.util.*;
 public class StackClass{
+    public static void pushAtBottom(int data, Stack<Integer> s){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+
+        int top = s.pop();
+        pushAtBottom(data, s);
+        s.push(top);
+    }
     public static void main(String[] args) {
-        Stack<Integer> s = new Stack<>();
+                Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
+
+        pushAtBottom(5, s);
+
         while (!s.isEmpty()) {
             System.out.println(s.peek());
             s.pop();
         }
     }
+
 }
