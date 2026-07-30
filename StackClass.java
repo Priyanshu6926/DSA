20530201dcfc31c84f0fb600852a1956a68e6b8e
Priyanshu6926 <priyanshushingole@gmail.com>
2026-07-31 00:34:49 +0530

    updated array list

--- StackClass.java
+++ StackClass.java
@@ -63,11 +63,11 @@
 //     }
 // }
 
-
+import java.util.ArrayList;
 public class StackClass{
-    class Stack{
+    static class Stack{
         ArrayList<Integer> list = new ArrayList<>();
-        public static boolean isEmpty(){
+        public boolean isEmpty(){
             return list.size() == 0;
         }
         //push
@@ -89,6 +89,19 @@ public class StackClass{
                 return -1;
             }
             return list.get(list.size()-1);
+        }
+    }
+    public static void main(String[] args) {
+
+        Stack s = new Stack();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
         }
     }
 }
