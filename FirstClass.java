436578d63fb7f776397bd87b80f402ea7f90a37b
Priyanshu6926 <priyanshushingole@gmail.com>
2026-07-30 21:50:39 +0530

    LC 26

--- FirstClass.java
+++ FirstClass.java
@@ -322,7 +322,7 @@ public class FirstClass{
         }
     }}
 // no is prime or not\
-mport java.util.*;
+import java.util.*;
 public class FirstClass{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
