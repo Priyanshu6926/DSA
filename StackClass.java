e3e00cc2d26ecddda86e9eb575eb5f29a304fa6c
Priyanshu6926 <priyanshushingole@gmail.com>
2026-07-31 00:43:25 +0530

    java collection framework

--- StackClass.java
+++ StackClass.java
@@ -63,42 +63,61 @@
 //     }
 // }
 
-import java.util.ArrayList;
+
+// // ARRAY LIST
+
+// import java.util.ArrayList;
+// public class StackClass{
+//     static class Stack{
+//         ArrayList<Integer> list = new ArrayList<>();
+//         public boolean isEmpty(){
+//             return list.size() == 0;
+//         }
+//         //push
+//         public void push(int data){
+//             list.add(data);
+//         }
+//         //pop
+//         public int pop(){
+//             if(isEmpty()) {
+//                 return -1;
+//             }
+//             int top = list.get(list.size()-1);
+//             list.remove(list.size()-1);
+//             return top;
+//         }
+//         //peek
+//         public int peek(){
+//             if(isEmpty()) {
+//                 return -1;
+//             }
+//             return list.get(list.size()-1);
+//         }
+//     }
+//     public static void main(String[] args) {
+
+//         Stack s = new Stack();
+
+//         s.push(1);
+//         s.push(2);
+//         s.push(3);
+
+//         while (!s.isEmpty()) {
+//             System.out.println(s.peek());
+//             s.pop();
+//         }
+//     }
+// }
+
+
+// JAVA COLLECETION Framnwork
+import java.util.*;
 public class StackClass{
-    static class Stack{
-        ArrayList<Integer> list = new ArrayList<>();
-        public boolean isEmpty(){
-            return list.size() == 0;
-        }
-        //push
-        public void push(int data){
-            list.add(data);
-        }
-        //pop
-        public int pop(){
-            if(isEmpty()) {
-                return -1;
-            }
-            int top = list.get(list.size()-1);
-            list.remove(list.size()-1);
-            return top;
-        }
-        //peek
-        public int peek(){
-            if(isEmpty()) {
-                return -1;
-            }
-            return list.get(list.size()-1);
-        }
-    }
     public static void main(String[] args) {
-
-        Stack s = new Stack();
-
+        Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
-
         while (!s.isEmpty()) {
             System.out.println(s.peek());
             s.pop();
