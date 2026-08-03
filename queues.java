b54f88192b76ae978436fdd2bd9af69947a8cdbd
Priyanshu6926 <priyanshushingole@gmail.com>
2026-08-03 15:16:15 +0530

    full queues

--- queues.java
+++ queues.java
@@ -1,46 +1,74 @@
 public class queues {
-    static class Queue{
+
+    static class Queue {
+
         static int arr[];
         static int size;
         static int rear = -1;
 
-        Queue(int n){
+        Queue(int n) {
             arr = new int[n];
-            this.size = n;
-
+            size = n;
         }
-        public static boolean isEmpty(){
+
+        // Check if queue is empty
+        public static boolean isEmpty() {
             return rear == -1;
-
         }
-        //enqueue
-        public static void add(int data){
-            if(rear == size -1){
-                System.out.println("Full queue");
+
+        // Enqueue
+        public static void add(int data) {
+            if (rear == size - 1) {
+                System.out.println("Full Queue");
                 return;
             }
-            rear ++;
+
+            rear++;
             arr[rear] = data;
+        }
 
-            //dequeue
-            public static int remove(){
-                if(isEmpty()){
-                    System.out.println("Empty queue");
-                    return -1;
-                }
-                int front = arr[0];
-                for(int i = 0; i< rear ;i++){
-                    arr[i] = arr[i+1];
-                }
-                rear--;
-                return front;
+        // Dequeue
+        public static int remove() {
+            if (isEmpty()) {
+                System.out.println("Empty Queue");
+                return -1;
             }
-        
 
+            int front = arr[0];
+
+            for (int i = 0; i < rear; i++) {
+                arr[i] = arr[i + 1];
+            }
+
+            rear--;
+
+            return front;
         }
 
-        public static void main(String[] args) {
-            
+        // Peek
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("Empty Queue");
+                return -1;
+            }
+
+            return arr[0];
         }
-    }   
+    }
+
+    public static void main(String[] args) {
+
+        Queue q = new Queue(5);
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
 }
