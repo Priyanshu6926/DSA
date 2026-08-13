fc726e0d0ac95bc2803e94c7c769a1fd92a93e3d
Priyanshu6926 <priyanshushingole@gmail.com>
2026-08-13 16:51:19 +0530

    postorder

--- Trees/trees.java
+++ Trees/trees.java
@@ -81,7 +81,73 @@
 //     }
 // }
 
+
+
 // inorder treversal
+
+
+// package Trees;
+// public class trees{
+//     static class Node{
+//         int data;
+//         Node left;
+//         Node right;
+
+//         Node(int data){
+//             this.data = data;
+//             this.left = null;
+//             this.right = null;
+//         }
+//     }
+
+//         static class BinaryTree{
+//             static int idx = -1;
+//             public static Node buildTree(int node[]){
+//                 idx++;
+//                 if(node[idx] == -1){
+//                     return null;
+//                 }
+//                 Node newNode = new Node(node[idx]);
+//                 newNode.left = buildTree(node);
+//                 newNode.right = buildTree(node);
+//                 return newNode;            
+//             }
+//         }
+
+//         public static void preorder(Node root){
+//             if(root == null){
+//                 return;
+//             }
+//             System.out.print(root.data+" ");
+//             preorder(root.left);
+//             preorder(root.right);
+//         }
+
+//         public static void inorder(Node root){
+//             if(root == null){
+//                 return;
+//             }
+//             inorder(root.left);
+//             System.out.print(root.data+" ");
+//             inorder(root.right);
+//         }
+
+//         public static void main(String[] args) {
+//             int node[] = {1,2,4,-1,-1,5,-1,-1,3,6,-1,-1,7,-1,-1};
+//             BinaryTree tree = new BinaryTree();
+//             Node root = tree.buildTree(node);
+            
+//             System.out.println(root.data);
+
+//             // calling inorder traversal
+//             inorder(root);
+//         }
+// }
+
+
+//post order traversal
+
+
 package Trees;
 public class trees{
     static class Node{
@@ -128,14 +194,24 @@ public class trees{
             inorder(root.right);
         }
 
+        public static void postorder(Node root){
+            if(root == null){
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data+" ");
+        }
         public static void main(String[] args) {
-            int node[] = {1,2,4,-1,-1,5,-1,-1,3,6,-1,-1,7,-1,-1};
+            int node[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
             BinaryTree tree = new BinaryTree();
             Node root = tree.buildTree(node);
             
             System.out.println(root.data);
 
             // calling inorder traversal
-            inorder(root);
+            postorder(root);
         }
 }
+
+
