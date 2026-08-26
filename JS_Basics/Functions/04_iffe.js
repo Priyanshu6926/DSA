7547ef9824e2ff314f8184e84bcd3806eb8bfdfe
Priyanshu6926 <priyanshushingole@gmail.com>
2026-08-27 00:43:10 +0530

    iffe

--- JS_Basics/Functions/04_iffe.js
+++ JS_Basics/Functions/04_iffe.js
@@ -1,6 +1,11 @@
 // Immediate Invoked Function Expression (IIFE)
 
 (function chai(){
+    //named iffe
     console.log("DB connected");
     
 })();
+
+((name) => {
+    console.log(`Database connected,${name}`);
+})("Priyanshu")
