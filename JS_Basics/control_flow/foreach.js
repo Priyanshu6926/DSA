a137595116181d4f3a0539c8b6b10eb49c74287e
Priyanshu6926 <priyanshushingole@gmail.com>
2026-09-26 16:53:47 +0530

    for each

--- JS_Basics/control_flow/foreach.js
+++ JS_Basics/control_flow/foreach.js
@@ -4,9 +4,9 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 //     console.log(val);
 // } )
 
-// coding.forEach( (item) => {
-//     console.log(item);
-// } )
+coding.forEach( (item) => {
+    console.log(item);
+} )
 
 // function printMe(item){
 //     console.log(item);
@@ -35,5 +35,5 @@ const myCoding = [
 
 myCoding.forEach( (item) => {
     
-    console.log(item.languageName);
+    // console.log(item.languageName);
 } )
